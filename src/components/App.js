65fc2b132dc9ca4e6bd0ad6ad99d2d4335279e7f
Abhishek Dam <33367709+abhishekdam@users.noreply.github.com>
2021-06-21 02:07:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from 'react';
+import './App.css';
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove the main div */}
+        <ol key="relativeList">
+          <li key="relativeListItem1">Suresh</li>
+          <li key="relativeListItem2">Suresh</li>
+          <li key="relativeListItem3">Suresh</li>
+          <li key="relativeListItem4">Suresh</li>
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
